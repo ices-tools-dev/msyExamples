e2ab8caab3a6ac35309a8ffe188c472eb50b3af4
colinpmillar <colinpmillar@googlemail.com>
2013-12-10 08:30:51 +0000

    progress on Examples document

--- codNS.R
+++ codNS.R
@@ -11,68 +11,78 @@ library(msy)
 load("data/codNS.rData")
 
 # ---- setupcod ----
-codsg <- 
-  list(
-    data = codNS,
-    wt.years = c(2008, 2012),
-    Fscan = seq(0, 1.5, len = 40),
-    Bpa = 150000,
-    Blim = 70000,
-    Btrigger = 150000)
+codsetup <- list(
+           data = codNS,
+           wt.years = c(2008, 2012),
+           Fscan = seq(0, 1.5, len = 40),
+           Bpa = 150000,
+           Blim = 70000,
+           Btrigger = 150000,
+           verbose = FALSE) # set verbose to TRUE if you want to see simulation progress
 
 # ---- codsegreg ----
 codsg <-
-  within(codsg,
+  within(codsetup,
   {
-    fit <- fitModels(codNS, nsamp = 2000, model = "segreg")
-    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan)
+    fit <- fitModels(data, nsamp = 2000, model = "segreg")
+    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, verbose = verbose)
     ref <- Eqplot(sim, fit, Blim = Blim, Bpa = Bpa, plot = FALSE)
   })
 
 # ---- getref1 ----
-codsg $ ref $ Refs
+t(codsg $ ref $ Refs)
 
+# ---- plotref1 ----
+with(codsg, Eqplot(sim, fit, Blim = Blim, Bpa = Bpa))
+
+# ---- plotsr1 ----
+SRplot(codsg $ fit)
+
+
+# ---- codsgfit ----
+str(codsg $ fit, 2)
+
+# ---- codsgsim ----
+str(codsg $ sim, 2)
 
 # ---- codbevholt ----
 codbh <-
-  within(codbh,
+  within(codsetup,
   {
-    fit <- fitModels(codNS, nsamp = 2000, model = "bevholt")
-    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan)
+    fit <- fitModels(data, nsamp = 2000, model = "bevholt")
+    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, verbose = verbose)
     ref <- Eqplot(sim, fit, Blim = Blim, Bpa = Bpa, plot = FALSE)
   })
 
 # ---- codricker ----
 codrk <-
-  within(codrk,
+  within(codsetup,
   {
-    fit <- fitModels(codNS, nsamp = 2000, model = "bevholt")
-    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan)
+    fit <- fitModels(data, nsamp = 2000, model = "bevholt")
+    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, verbose = verbose)
     ref <- Eqplot(sim, fit, Blim = Blim, Bpa = Bpa, plot = FALSE)
   })
 
 # ---- codall ----
 codall <-
-  within(codall,
+  within(codsetup,
   {
-    fit <- fitModels(codNS, nsamp = 2000, model = c("segreg", "bevholt","ricker"))
-    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan)
+    fit <- fitModels(data, nsamp = 2000, model = c("segreg", "bevholt","ricker"))
+    sim <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, verbose = verbose)
     ref <- Eqplot(sim, fit, Blim = Blim, Bpa = Bpa, plot = FALSE)
   })
 
 
 # ---- codallvar ----
 codall_variations <-
-  within(codall,
+  within(codall, 
   {
     # simulate without process error in the recruitment predictions
-    sim2 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, process.error = FALSE, verbose = FALSE)
-    # simulate with process error (i.e. using predictive distrution of recruitment)
-    # and include a simple HCR
-    sim3 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, Btrigger = Btrigger, verbose = FALSE)
-    # simulate without process error (i.e. using model and parameter error only, not including "observation" error)
-    # and include a simple HCR
-    sim4 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, process.error = FALSE, Btrigger = Btrigger, verbose = FALSE)
+    sim2 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, process.error = FALSE, verbose = verbose)
+    # simulate with process error (i.e. using predictive distrution of recruitment) and include a simple HCR
+    sim3 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, Btrigger = Btrigger, verbose = verbose)
+    # simulate without process error (i.e. using model and parameter error only, not including "observation" error) and include a simple HCR
+    sim4 <- EqSim(fit, wt.years = wt.years, Fscan = Fscan, process.error = FALSE, Btrigger = Btrigger, verbose = verbose)
 
     # now calculate the reference points for each simulation
     ref2 <- Eqplot(sim2, fit, Blim = Blim, Bpa = Bpa, plot = FALSE)
